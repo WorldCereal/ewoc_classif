ec07ef3fdee8a2e645003c2364ba4c5af4f996b1
Benatia Fahd <18417413+Fahdben@users.noreply.github.com>
2021-12-13 15:42:03 +0100

    update notify_vdm

--- src/ewoc_classif/utils.py
+++ src/ewoc_classif/utils.py
@@ -5,18 +5,19 @@ import requests
 _logger = logging.getLogger(__name__)
 
 
-def notify_vdm(stac_json, endpoint_url ="endpoint_url"):
+def notify_vdm(stac_json, endpoint_url,user_token):
     """
     Notify the VDM that a new ewoc product is available
     This function will send a post request to the VDM REST API
     :param stac_json: Json file or dictionary, output of the classifier
     :param url: VDM REST API endpoint url
-    :return: REST API status code, 200: all good
+    :return: REST API status code and response body as json 200: all good
     """
-    response = requests.post(url = endpoint_url, json=stac_json)
+    headers = {'content-type': 'application/json','X-User-Info': user_token}
+    response = requests.post(url = endpoint_url, json=stac_json, headers=headers)
     status_code = response.status_code
     if  status_code == 200:
         _logger.info('Successful notification to VDM, well done!')
     else:
         _logger.error(f'Error for VDM notification. Error code: {status_code}')
-    return status_code
+    return status_code, response.json()
