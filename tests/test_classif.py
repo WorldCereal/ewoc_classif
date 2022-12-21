79220581ed4fa0d9f9499668b9581621b5fd2b0f
msavinaud <mickael.savinaud@csgroup.eu>
2022-12-21 17:42:39 +0100

    add more tests

--- tests/test_classif.py
+++ tests/test_classif.py
@@ -92,8 +92,9 @@ class Test_classif(unittest.TestCase):
         ewoc_detector=EWOC_CROPTYPE_DETECTOR,
         ewoc_season=EWOC_SUPPORTED_SEASONS[1])
 
-    #@unittest.skipIf(os.getenv("EWOC_TEST_VAL_TEST") is None,"env variable not set")
-    def test_run_classif_croptype_summer1_15STU_119_extra_TIR_gap(self):
+    # TODO: fix the test to succeed
+    @unittest.skipIf(os.getenv("EWOC_TEST_VAL_TEST") is None,"env variable not set")
+    def test_run_classif_croptype_summer1_15STU_119_with_larger_TIR_gap(self):
         """ This test no more failed due to the increase of the maxgap for TIR (300 instead 60)"""
         os.environ['EWOC_COLL_MAXGAP_TIR']='300'
         print(os.environ['EWOC_COLL_MAXGAP_TIR'])
@@ -104,7 +105,6 @@ class Test_classif(unittest.TestCase):
         ewoc_detector=EWOC_CROPTYPE_DETECTOR,
         ewoc_season=EWOC_SUPPORTED_SEASONS[1])
 
-    #@unittest.skipIf(os.getenv("EWOC_TEST_VAL_TEST") is None,"env variable not set")
     def test_run_classif_croptype_summer1_55LBE_1(self):
         """ No cropland pixels, therefore block is write with nodata 255 value
         """
@@ -115,4 +115,105 @@ class Test_classif(unittest.TestCase):
         ewoc_detector=EWOC_CROPTYPE_DETECTOR,
         ewoc_season=EWOC_SUPPORTED_SEASONS[1])
 
-     # EWOC_COLL_MAXGAP_TIR=300 18MYT c728b264-5c97-4f4c-81fe-1500d4c4dfbd_20090_20221027083824 --block-ids 108 --ewoc-detector croptype --ewoc-season summer2
+     # EWOC_COLL_MAXGAP_TIR=300 18MYT 
+     # c728b264-5c97-4f4c-81fe-1500d4c4dfbd_20090_20221027083824 
+     # --block-ids 108 
+     # --ewoc-detector croptype 
+     # --ewoc-season summer2
+
+    @unittest.skipIf(os.getenv("EWOC_TEST_VAL_TEST") is None,"env variable not set")
+    def test_run_classif_cropland_01KFS_60(self):
+        """ This test failed due to the maxgap issue on SAR (108 instead 60)
+        """
+        run_classif('01KFS',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_5049_20220926141536',
+        block_ids=[60],
+        upload_block=False)
+
+    # TODO: fix the test to succeed
+    @unittest.skipIf(os.getenv("EWOC_TEST_VAL_TEST") is None,"env variable not set")
+    def test_run_classif_cropland_01KFS_60_with_larger_SAR_gap(self):
+        """ This test no more failed due to the increase of the maxgap for SAR (120 instead 60)
+        """
+        os.environ['EWOC_COLL_MAXGAP_SAR']='120'
+        run_classif('01KFS',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_5049_20220926141536',
+        block_ids=[60],
+        upload_block=False)
+
+    def test_run_classif_cropland_15PXR_110(self):
+        """ This test failed due to the maxgap issue on OPTICAL (160 instead 60) on this block
+        """
+        run_classif('15PXR',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_33117_20220823152041',
+        block_ids=[110],
+        upload_block=False)
+
+    @unittest.skipIf(os.getenv("EWOC_TEST_VAL_TEST") is None,"env variable not set")
+    def test_run_classif_cropland_15PXR_1(self):
+        """ This test succeed over this block
+        """
+        run_classif('15PXR',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_33117_20220823152041',
+        block_ids=[1],
+        upload_block=False)
+
+    def test_run_classif_cropland_54VWM_106(self):
+        """ This test failed due to the maxgap issue on OPTICAL (61 instead 60) on this block (close to coastal area)
+        """
+        run_classif('54VWM',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_17163_20221114214029',
+        block_ids=[106],
+        upload_block=False)
+
+    def test_run_classif_cropland_43PCM_0(self):
+        """ This test failed due to the maxgap issue on OPTICAL (237 instead 60) on this block (island case)
+        """
+        run_classif('43PCM',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_28107_20220921094801',
+        block_ids=[0],
+        upload_block=False)
+
+    def test_run_classif_cropland_38UPD_120(self):
+        """ This test failed due to the maxgap issue on OPTICAL (124 instead 60) on this block (land case)
+        """
+        run_classif('38UPD',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_22194_20220908152510',
+        block_ids=[120],
+        upload_block=False)
+
+    @unittest.skipIf(os.getenv("EWOC_TEST_VAL_TEST") is None,"env variable not set")
+    def test_run_classif_cropland_36UVB_120(self):
+        """ Must failed but not the case
+        UKR tile
+        """
+        run_classif('36UVB',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_22190_20220929210524',
+        block_ids=[120],
+        upload_block=False)
+
+    def test_run_classif_cropland_38UQB_1(self):
+        """ This test failed due to the maxgap issue on OPTICAL (68 instead 60) on this block (land case)
+        """
+        run_classif('38UQB',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_22096_20220906224410',
+        block_ids=[1],
+        upload_block=False)
+
+    @unittest.skipIf(os.getenv("EWOC_TEST_VAL_TEST") is None,"env variable not set")
+    def test_run_classif_cropland_39UXT_60(self):
+        """ Must fail but not the case
+        """
+        run_classif('39UXT',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_22096_20220906224410',
+        block_ids=[60],
+        upload_block=False)
+
+    @unittest.skipIf(os.getenv("EWOC_TEST_VAL_TEST") is None,"env variable not set")
+    def test_run_classif_cropland_43SCB_50(self):
+        """ Must fail but not the case
+        """
+        run_classif('43SCB',
+        'c728b264-5c97-4f4c-81fe-1500d4c4dfbd_25147_20220918052128',
+        block_ids=[50],
+        upload_block=False)
